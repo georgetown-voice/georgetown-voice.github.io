3daddf62089b0e821d0d9c8f93b4c45bbf902415
alexclewontin <alex.c.lewontin@gmail.com>
2018-03-21 03:05:22 -0400

    shortcode work

--- functions.php
+++ functions.php
@@ -1,10 +1,35 @@
 <?php
-	func mappy_js_handler ($atts, $content = null) {
-		returns "<link><div id=\"mapid\"></div><div id=\"target\"></div><script src=\"whatever.js\"></script>";
+	function initialize_mappy ($atts) {
+		
+		$a = shortcode_atts( array(
+			'lat' => 'LAT HERE',
+			'long' => 'LONG HERE',
+			'zoom' => '13'
+		), $atts );
+		
+		ob_start();
+		?>
+		<link rel="stylesheet" type="text/css" href="mappyStyle.css">
+		<script src="mappy.js" type="text/javascript"></script>
+		<div id="mapid"></div>
+		<div id="target"></div>
+		<?php
+		return ob_get_clean();
+
 	}
-	func mappy_item_handler($atts, $content = null) {
-		// id, name, image, copy, author
-		// calls function from whatever.js that registers this as a point-able thing.
+	add_shortcode( 'initialize mappy', 'initialize_mappy' );
+	
+	function mappy_item_handler($atts, $content = null) {
+		
+		$a = shortcode_atts( array(
+			'id' => 'McLovin',
+			'title' => 'Super Bad',
+			'image_address' => 'https://www.ebay.com/itm/McLovin-Superbad-Movie-Joke-ID-0A-/262482881654',
+			'author' => 'Seth Rogan',
+			'lat' => 'LAT HERE',
+			'long' => 'LONG HERE'
+		), $atts);
+
 		return;
 	}
 
--- mappy.js
+++ mappy.js
@@ -1,29 +1,30 @@
 
 //initialize the map, get the tiles from mapbox with Alex Lewontin's token
 
-var mymap = L.map('mapid').setView([38.907, -77.037], 13);
-            
-L.tileLayer('https://api.mapbox.com/styles/v1/alexclewontin/cjercgc6y67en2snxyb8qm2su/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWxleGNsZXdvbnRpbiIsImEiOiJjamVxcDE3c28wNnkwMnFteXcyZnlwZHR2In0.ibNOkvue-_GppjJdUEhQKg', 
+function initializeMap(lat, long, zoom)
 {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: 'pk.eyJ1IjoiYWxleGNsZXdvbnRpbiIsImEiOiJjamVxcDE3c28wNnkwMnFteXcyZnlwZHR2In0.ibNOkvue-_GppjJdUEhQKg'
-}).addTo(mymap);
-
+    var mymap = L.map('mapid').setView([lat, long], zoom);
+            
+    L.tileLayer('https://api.mapbox.com/styles/v1/alexclewontin/cjercgc6y67en2snxyb8qm2su/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWxleGNsZXdvbnRpbiIsImEiOiJjamVxcDE3c28wNnkwMnFteXcyZnlwZHR2In0.ibNOkvue-_GppjJdUEhQKg', 
+    {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: 'pk.eyJ1IjoiYWxleGNsZXdvbnRpbiIsImEiOiJjamVxcDE3c28wNnkwMnFteXcyZnlwZHR2In0.ibNOkvue-_GppjJdUEhQKg'
+    }).addTo(mymap);
+}
 
 
 //define the relevant class
 
 class mapPin {
-    constructor(id, name, author, imageAddress, copy, long, lat) 
+    constructor(title, author, imageAddress, copy, lat, long) 
     {
-        this.id = id;
         mapPin.instances.push(this);
-        this.name = name;
+        this.title = title;
         this.author = author;
         this.image = imageAddress;
         this.copy = copy;
-        this.mapMarker = L.marker([long, lat], {riseOnHover: true, opacity: .5}).addTo(mymap);
+        this.mapMarker = L.marker([lat, long], {riseOnHover: true, opacity: .5}).addTo(mymap);
     }
 
 }
@@ -44,8 +45,11 @@ mapPin.on('mouseover', function(e)
 
 function changePost(mapPin)
 {
-    var template = $('#template').html();
+    
+    $.get('mappyTemplate.mst', function(template){
+    //var template = $('#template').html();
     Mustache.parse(template);
-    var output = Mustache.render(template, mapPin)
+    var output = Mustache.render(template, mapPin);
     $("#target").html(output);
+    })
 }
